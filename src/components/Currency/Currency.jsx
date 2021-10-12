0dbf988ab504adc78f8f8250ed38d3e3d41d5b44
Victoria <sivosha2006@mail.ru>
2021-10-12 20:05:18 +0300

    added apollo client

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-// import dollar from './dollar.png'
 import './Currency.css'
+
 
 class Currency extends Component {
   render() {
     return (
       <select className="currency-change">
         <option value=""></option>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="JPY">JPY</option>
+        <option value="USD">$ USD</option>
+        <option value="EUR">€ EUR</option>
+        <option value="JPY">¥ JPY</option>
       </select>
     );
   }
